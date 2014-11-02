f38172504cec941bb21611c04b48ed2be4b7a95c
nurilumam <nuril.umam@gmail.com>
2014-11-02 17:23:22 +0700

    test
    test

---
+++ application/config/cms_config.php
@@ -0,0 +1,15 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['cms_table_prefix'] = '';
+$config['max_menu_depth'] = 10;
+
+$config['site_layout'] = 'default';
+$config['__cms_version'] = '0.7.0-beta';
+$config['site_language'] = 'english';
+$config['site_theme'] = 'default';
+$config['cms_google_analytic_property_id'] = '';
+$config['site_favicon'] = '{{ base_url }}assets/nocms/images/No-CMS-favicon.png';
+$config['site_name'] = 'No-CMS';
+$config['site_footer'] = 'Powered by No-CMS &copy; 2013';
+$config['site_logo'] = '{{ base_url }}assets/nocms/images/No-CMS-logo.png';
+$config['site_slogan'] = 'A Free CodeIgniter Based CMS Framework';
---
+++ application/logs/log-2014-10-01.php
@@ -0,0 +1,81 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2014-10-01 14:09:49 --> Unable to connect to the database
+ERROR - 2014-10-01 14:09:49 --> Unable to connect to the database
+ERROR - 2014-10-01 14:10:08 --> Unable to connect to the database
+ERROR - 2014-10-01 14:10:08 --> Unable to connect to the database
+ERROR - 2014-10-01 14:10:08 --> Unable to connect to the database
+ERROR - 2014-10-01 14:10:08 --> Unable to connect to the database
+ERROR - 2014-10-01 14:10:08 --> Unable to connect to the database
+ERROR - 2014-10-01 14:10:08 --> Unable to connect to the database
+ERROR - 2014-10-01 14:10:08 --> Unable to connect to the database
+ERROR - 2014-10-01 14:10:08 --> Unable to connect to the database
+ERROR - 2014-10-01 14:10:08 --> Unable to connect to the database
+ERROR - 2014-10-01 14:10:08 --> Unable to connect to the database
+ERROR - 2014-10-01 14:10:08 --> Unable to connect to the database
+ERROR - 2014-10-01 14:10:08 --> Unable to connect to the database
+ERROR - 2014-10-01 14:10:09 --> Unable to connect to the database
+ERROR - 2014-10-01 14:10:09 --> Unable to connect to the database
+ERROR - 2014-10-01 14:10:09 --> Unable to connect to the database
+ERROR - 2014-10-01 14:10:39 --> Unable to connect to the database
+ERROR - 2014-10-01 14:26:39 --> Severity: Error --> Maximum execution time of 30 seconds exceeded C:\xampp\htdocs\SIPN\system\core\Router.php 27
+ERROR - 2014-10-01 14:34:20 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:34:30 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:34:37 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:37:39 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:37:39 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:37:39 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:37:39 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:37:39 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:37:39 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:37:43 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:37:43 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:37:43 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:38:40 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:38:40 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:38:40 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:38:59 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:38:59 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:38:59 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:39:24 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:39:24 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:39:24 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:39:24 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:39:38 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:39:38 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:39:38 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:39:46 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:39:46 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:39:50 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:39:50 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:39:54 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:39:54 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:39:54 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:39:54 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:40:02 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:40:02 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:40:02 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:42:45 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:42:45 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:42:45 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:43:20 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:43:20 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:43:20 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:43:47 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:43:47 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:43:47 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:46:12 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:46:12 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:46:12 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:47:50 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:47:50 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:47:50 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:47:59 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:47:59 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:48:00 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:48:04 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:48:04 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:48:04 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:48:25 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:48:25 --> 404 Page Not Found: 
+ERROR - 2014-10-01 14:48:25 --> 404 Page Not Found: 
